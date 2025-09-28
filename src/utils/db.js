e3ba98489443812a3788e72ead218552d6ff110d
Umair Amjad <umairamjad970@gmail.com>
2025-09-28 14:49:55 -0700

    Fixed blog and login issues

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,12 +1,28 @@
+// import mongoose from "mongoose";
+
+// const connect = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO);
+//   } catch (error) {
+//     throw new Error("Connection Failed!");
+//   }
+// };
+
+// export default connect;
+
 import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
+    if (mongoose.connections[0].readyState) return; // avoid multiple connections
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected!");
   } catch (error) {
+    console.error("MongoDB Connection Failed!", error);
     throw new Error("Connection Failed!");
   }
 };
 
-
 export default connect;
+
+// NEXTAUTH_URL=http://localhost:3000
